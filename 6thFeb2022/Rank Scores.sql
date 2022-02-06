4a860d79be1f29ef5b33833b0165fc90f90fe534
Surya742 <suryakumarsahani.742@gmail.com>
2022-02-06 21:01:21 +0530

    solved Rank Scores

---
+++ 6thFeb2022/Rank Scores.sql
@@ -0,0 +1,25 @@
+-- Rank Scores
+
+-- SQL Schema
+-- Table: Scores
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | score       | decimal |
+-- +-------------+---------+
+-- id is the primary key for this table.
+-- Each row of this table contains the score of a game. Score is a floating point value with two decimal places.
+ 
+
+-- Write an SQL query to rank the scores. The ranking should be calculated according to the following rules:
+
+-- The scores should be ranked from the highest to the lowest.
+-- If there is a tie between two scores, both should have the same ranking.
+-- After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+-- Return the result table ordered by score in descending order.
+
+-- Prob Link: https://leetcode.com/problems/rank-scores/
+
+-- select score, dense_rank() over(order by score desc) as "rank" from Scores;
